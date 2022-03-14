42a01003ce203d0354fc1b88e4eb38efee86bb51
karishmarajput <karrajput3948@gmail.com>
2022-03-14 18:33:10 +0530

    Add basic layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Profile from './components/Profile.js';
+import Dashboard from './components/Dashboard.js';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="members/ids" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
