6918616606c74f0c0608a4120ac635296cad06f0
karishmarajput <karrajput3948@gmail.com>
2022-03-14 20:59:55 +0530

    update Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import ResponsiveDrawer from './Sidebar.js';
 import data from './profile.json';
+import './profile.css';
 export default function Profile() {
   data = data.profile[0];
-  console.log(data.img)
+  console.log(data.img);
   return (
     <div>
       <ResponsiveDrawer />
-      <div>
-        <div>
-          <h1>{data.name}</h1>
+      <div className="profile-div">
+        <div className="profile-main-div">
+          <div className="profile-main-div-name">
+            <h1>{data.name}</h1>
+          </div>
+          <div className="profile-main-div-image">
+            <img
+              src={data.img}
+              width="250"
+              width="250"
+              className="profileImage"
+            />
+          </div>
         </div>
-        <div>
-          <image src={data.img} />
-        </div>
-        wadsnhjmashk w jkasjkajswef egrere
       </div>
     </div>
   );
