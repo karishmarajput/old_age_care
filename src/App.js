ca5296924e3dd911383f631cdc0f07c4cf17ec26
karishmarajput <karrajput3948@gmail.com>
2022-03-16 20:58:04 +0530

    4 files modified

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './style.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Profile from './components/Profile.js';
+import MembersList from './components/MembersList.js';
 import Dashboard from './components/Dashboard.js';
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="members/ids" element={<Profile />} />
+        <Route path="memberslist" element={<MembersList />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/MembersList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ResponsiveDrawer from './Sidebar.js';
+import data from './profile.json';
+export default function MembersList() {
+  return (
+    <div>
+      <ResponsiveDrawer />
+      hello 
+    </div>
+  );
+}
