990244b73c959c1faed561048f9c2edfce27239a
karishmarajput <karrajput3948@gmail.com>
2022-03-16 21:17:36 +0530

    1 file modified, 1 added, 1 deleted

--- src/components/MembersList.js
+++ src/components/MembersList.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import ResponsiveDrawer from './Sidebar.js';
 import data from './profile.json';
+import './membersList.css';
 export default function MembersList() {
+  const member = data.profile.map((member) => {
+    return (
+      <tr>
+        <td>{member.id}</td>
+        <td>{member.name}</td>
+        <td>{member.familyNumber}</td>
+        <td>{member.dob} </td>
+      </tr>
+    );
+  });
   return (
     <div>
       <ResponsiveDrawer />
-      hello 
+      <table>
+        <tbody>
+          <tr>
+            <th>Sr. no</th>
+            <th>Name</th>
+            <th>Family Number</th>
+            <th>D.O.B</th>
+          </tr>
+          {member}
+        </tbody>
+      </table>
     </div>
   );
 }
