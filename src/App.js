62ed2a82c010ad706fa40158b0608b48c701ba12
karishmarajput <karrajput3948@gmail.com>
2022-03-24 22:17:26 +0530

    2 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Profile from './components/Profile.js';
 import MembersList from './components/MembersList.js';
 import Dashboard from './components/Dashboard.js';
+import Inventory from './components/Inventory.js';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/" element={<Dashboard />} />
         <Route path="members/ids" element={<Profile />} />
         <Route path="memberslist" element={<MembersList />} />
+        <Route path="inventory" element={<Inventory />} />
       </Routes>
     </BrowserRouter>
   );
